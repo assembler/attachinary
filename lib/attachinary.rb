e9c56ae47583b3e49f3e1cf79a890c0d7a26c365
Milovan Zogovic <milovan.zogovic@gmail.com>
2012-07-01 08:56:07 +0200

    extend ActiveRecord::Base on load

--- lib/attachinary.rb
+++ lib/attachinary.rb
@@ -1,4 +1,7 @@
-require "attachinary/engine"
+require 'attachinary/engine'
+
+require 'attachinary/active_record_extension'
+ActiveRecord::Base.extend Attachinary::ActiveRecordExtension
 
 module Attachinary
 end
