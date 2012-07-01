7f0f506180eb15063420ee906cfd6636e8d958e7
Milovan Zogovic <milovan.zogovic@gmail.com>
2012-07-01 08:56:58 +0200

    moved out from engine initialization

--- lib/attachinary/engine.rb
+++ lib/attachinary/engine.rb
@@ -1,15 +1,8 @@
-require 'attachinary/active_record_extension'
 require 'attachinary/view_helpers'
 
 module Attachinary
   class Engine < ::Rails::Engine
     isolate_namespace Attachinary
-
-    initializer "attachinary.extend_active_record" do |app|
-      ActiveSupport.on_load :active_record do
-        extend ActiveRecordExtension
-      end
-    end
 
     initializer "attachinary.include_view_helpers" do |app|
       ActiveSupport.on_load :action_view do
