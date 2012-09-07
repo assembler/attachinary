4e50e520ffd5c9c7e8ac0d735f33840345798e94
Milovan Zogovic <milovan.zogovic@gmail.com>
2012-09-07 14:51:28 +0200

    done through initializer

--- lib/attachinary.rb
+++ lib/attachinary.rb
@@ -1,10 +1,4 @@
 require 'attachinary/engine'
 
-unless defined?(ATTACHINARY_ORM)
-  ATTACHINARY_ORM = (ENV["ATTACHINARY_ORM"] || :active_record).to_sym
-end
-
-require "attachinary/orm/#{ATTACHINARY_ORM}"
-
 module Attachinary
 end
