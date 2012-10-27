a4641bc89763b418fe301d227e859cc2d2c662f2
Milovan Zogovic <milovan.zogovic@gmail.com>
2012-10-27 15:30:32 +0200

    let me do the "test" thing :)

--- spec/dummy/app/active_record/note.rb
+++ spec/dummy/app/active_record/note.rb
@@ -3,7 +3,7 @@ class Note < ActiveRecord::Base
   has_attachment :photo, accept: [:jpg, :png, :gif]
   has_attachments :images, accept: [:jpg, :png, :gif], maximum: 3
 
-  validates :body, presence: true, length: { minimum: 5, maximum: 128 }
+  validates :body, presence: true, length: { minimum: 4, maximum: 128 }
   validates :photo, presence: true
 
   attr_accessible :body
