cdc27ae2b6e23bed217305e8d654b5c4530f91c0
Milovan Zogovic <milovan.zogovic@gmail.com>
2012-10-27 12:09:19 +0200

    including assets in vendor directory

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -13,7 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //=
-//= require cloudinary
+//= require jquery.ui.widget
+//= require jquery.iframe-transport
+//= require jquery.fileupload
+//= require cloudinary/jquery.cloudinary
 //= require attachinary
 //=
 //= require_tree .
