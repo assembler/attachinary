75c9d27b3ed698e4ab8d607f7e46ff4c5de3689d
Scott Rocher <scottrocher@gmail.com>
2014-01-11 15:26:30 -0500

    Remote as_json 'root' since Rails 4 has false as the default

--- lib/attachinary/orm/file_mixin.rb
+++ lib/attachinary/orm/file_mixin.rb
@@ -2,13 +2,12 @@ module Attachinary
   module FileMixin
     def self.included(base)
       base.validates :public_id, :version, :resource_type, presence: true
-      #base.attr_accessible :public_id, :version, :width, :height, :format, :resource_type
       base.after_destroy :destroy_file
       base.after_create  :remove_temporary_tag
     end
 
     def as_json(options)
-      super(only: [:id, :public_id, :format, :version, :resource_type], methods: [:path], root: false)
+      super(only: [:id, :public_id, :format, :version, :resource_type], methods: [:path])
     end
 
     def path(custom_format=nil)
