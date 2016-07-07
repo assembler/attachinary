67545628f06e5e5d02daa4cd7fb571a8aaf3c691
Adam Griffis <abgriff@gmail.com>
2016-07-07 13:10:24 -0500

    Revert "Update from main master"

--- spec/dummy4/app/views/notes/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@notes) do |note|
-  json.extract! note, :id, :body
-  json.url note_url(note, format: :json)
-end
--- spec/dummy4/config/boot.rb
+++
@@ -1,7 +0,0 @@
-unless defined?(ATTACHINARY_ORM)
-  ATTACHINARY_ORM = (ENV["ATTACHINARY_ORM"] || :active_record).to_sym
-end
-
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
-require 'bundler/setup' # Set up gems listed in the Gemfile.
