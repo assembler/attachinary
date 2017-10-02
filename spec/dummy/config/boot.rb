5e91e5877374ec50e7e6183f48577f699fc64f1b
Fabio Napoleoni <f.napoleoni@gmail.com>
2017-10-02 23:27:09 +0200

    Fix dummy application boot

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,7 +1,9 @@
 unless defined?(ATTACHINARY_ORM)
-  ATTACHINARY_ORM = (ENV["ATTACHINARY_ORM"] || :active_record).to_sym
+  ATTACHINARY_ORM = (ENV['ATTACHINARY_ORM'] || :active_record).to_sym
 end
 
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+# Set up gems listed in the Gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../Gemfile', __FILE__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+$LOAD_PATH.unshift File.expand_path('../../../../lib', __FILE__)
