d613790fc8c77b700d91c5957abe3c8668835b14
Mike Costanza <mike.costanza@reverb.com>
2019-04-26 13:32:00 -0500

    Fix SimpleForm deprecation warnings

--- lib/attachinary/simple_form.rb
+++ lib/attachinary/simple_form.rb
@@ -1,7 +1,7 @@
 class AttachinaryInput < SimpleForm::Inputs::Base
   attr_reader :attachinary_options
 
-  def input
+  def input(_wrapper_options = nil)
     template.builder_attachinary_file_field_tag attribute_name, @builder, { html: input_html_options }.merge(options)
   end
 end
